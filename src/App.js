81cb4c6c59473320604862aa8225297f14ee4f83
unknown <ggg7152@gmail.com>
2022-01-17 23:46:34 +0900

    메인 ui 추가

---
+++ src/App.js
@@ -0,0 +1,96 @@
+/* eslint-disable */
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Jumbotron,
+  fluid,
+  Button,
+  Navbar,
+  Container,
+  Nav,
+  NavDropdown,
+} from "react-bootstrap";
+import Data from "./data.js"; //다른 파일 import
+
+function App() {
+
+  let [items, setItems] = useState(Data);
+
+  return (
+    <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">coffeeShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <div className="main">
+        <h1>30% Season Off</h1>
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
+        </p>
+        <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          {
+            items.map((item)=>{
+              return (
+                <Card item={item}></Card>
+              ) 
+            })
+          }
+
+          <div className="col-md-4">
+            <img src="../img/coffee2.jpg" height="220px" width="100%" />
+            <h4>{items[1].title}</h4>
+            <p>{items[1].content}</p>
+          </div>
+          <div className="col-md-4">
+            <img src="../img/coffee3.jpg" height="220px" width="100%" />
+            <h4>{items[2].title}</h4>
+            <p>{items[2].content}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Card(props){ 
+    return(
+      <div className="col-md-4">
+        <img src={props.items.img} className="items" />
+        <h4>{props.items.title}</h4>
+        <p>{props.items.content}</p>
+      </div>
+    )
+}
+
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+    {
+      id : 0,
+      title : "White and Black",
+      content : "Born in France",
+      price : 120000,
+      img : "../img/coffee1.jpg"
+    },
+  
+    {
+      id : 1,
+      title : "Red Knit",
+      content : "Born in Seoul",
+      price : 110000,
+      img : "../img/coffee2.jpg"
+    },
+  
+    {
+      id : 2,
+      title : "Grey Yordan",
+      content : "Born in the States",
+      price : 130000,
+      img : "../img/coffee3.jpg"
+    }
+  ] 
